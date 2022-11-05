a6065ca7a6ac44990789b5d13def1acc2cb7260a
DieWerkself <redead29@gmail.com>
2022-11-05 16:29:19 +0300

    Sets

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -62,6 +62,37 @@ Pizza is prepared with ${mainIngridient}${other}`);
   },
 };
 
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(ordersSet);
+
+console.log(new Set('Dmitry'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const ordersStaff = [...new Set(staff)];
+console.log(ordersStaff);
+console.log(new Set(staff).size);
+
+console.log(new Set('dmitrysemenov').size);
 /*
 //// Looping objects
 // Property NAMES
