d00f8092c91d4766f8d51670245385969c3ad9c8
DieWerkself <redead29@gmail.com>
2022-11-05 21:02:05 +0300

    Maps

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -62,6 +62,40 @@ Pizza is prepared with ${mainIngridient}${other}`);
   },
 };
 
+/// Maps
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 10;
+console.log(rest.get(time >= rest.get('open') && time <= rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+//// Sets
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -93,7 +127,7 @@ console.log(ordersStaff);
 console.log(new Set(staff).size);
 
 console.log(new Set('dmitrysemenov').size);
-/*
+
 //// Looping objects
 // Property NAMES
 const properties = Object.keys(openingHours);
