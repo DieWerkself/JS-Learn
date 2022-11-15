fcd015a8f8b836b0b40f525fc329f1243fee81f7
DieWerkself <redead29@gmail.com>
2022-11-15 21:21:40 +0300

    Value vs. Reference

--- Functions/script.js
+++ Functions/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +25,39 @@ createBooking('LH123', 4);
 createBooking('LH123', 10);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const dmitry = {
+  name: 'Dmitry Semenov',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = `Mr. ${passenger.name}`;
+
+  if (passenger.passport === 123456789) {
+    alert('Check In');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, dmitry);
+// console.log(flight);
+// console.log(dmitry);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = dmitry;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000000);
+};
+
+newPassport(dmitry);
+checkIn(flight, dmitry);
+
+console.log(flight);
+console.log(dmitry);
