40d0e1ca9edfcfdc02368ed5dd14835476e7ba7e
DieWerkself <redead29@gmail.com>
2022-11-20 18:57:25 +0300

    The bind Method

--- Functions/script.js
+++ Functions/script.js
@@ -159,3 +159,44 @@ const flightData = [583, 'Martin Tyler'];
 
 book.call(swiss, ...flightData);
 console.log(swiss);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Dmitry Semenov');
+bookEW23('Sam Fisher');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23
+
+console.log(addVAT(256));
+console.log(addVAT(23));
+
+// Challange
+
+const addTaxRate = rate => value => value + value * rate;
+const addVAT2 = addTaxRate(0.23);
+
+console.log(addVAT2(256));
