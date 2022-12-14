864ee919456e80989e76fe8b35e9adcb32fe5fa1
DieWerkself <redead29@gmail.com>
2022-12-14 18:28:13 +0300

    The new AT Method

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -108,3 +108,15 @@ console.log([...arr, ...arr2]);
 // Join
 console.log(letters.join(' - '));
 */
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-2));
+
+console.log('Dmitry'.at(0));
+console.log('Dmitry'.at(-1));
