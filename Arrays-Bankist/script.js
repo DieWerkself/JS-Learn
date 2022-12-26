f1369d66aca0ff29d3df09b1ac13e25cab9fdcea
DieWerkself <redead29@gmail.com>
2022-12-26 18:26:42 +0300

    The map Method

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -185,3 +185,25 @@ currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
 */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const convert = movements.map(mov => Math.trunc(mov * eurToUsd));
+
+console.log(movements);
+console.log(convert);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementDescriptions);
