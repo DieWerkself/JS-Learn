55539c810f3df91213e1f57c282b01b7d7d7ab70
DieWerkself <redead29@gmail.com>
2022-12-27 18:56:27 +0300

    The filter Method

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -93,7 +93,6 @@ const createUsernames = accs => {
 };
 
 createUsernames(accounts);
-console.log(account1);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -105,7 +104,7 @@ console.log(account1);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -221,3 +220,15 @@ const movementDescriptions = movements.map(
 
 console.log(movementDescriptions);
 */
+
+const deposits = movements.filter(mov => mov > 0);
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
