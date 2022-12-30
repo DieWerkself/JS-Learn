2254a0d8c175c0c4914d3d1d1cf0755f1541194a
DieWerkself <redead29@gmail.com>
2022-12-30 20:54:52 +0300

    Coding Challenge #2

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -277,7 +277,7 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
-*/
+
 
 // Maximum value
 const max = movements.reduce((acc, mov) => {
@@ -285,3 +285,4 @@ const max = movements.reduce((acc, mov) => {
   else return mov;
 }, movements[0]);
 console.log(max);
+*/
