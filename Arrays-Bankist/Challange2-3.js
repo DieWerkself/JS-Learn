d71fe48dd525211c76d2b1bb2dd1d28399d1686d
DieWerkself <redead29@gmail.com>
2023-01-02 16:59:19 +0300

    Coding Challenge #3

--- Arrays-Bankist/Challange2-3.js
+++ Arrays-Bankist/Challange2-3.js
@@ -21,13 +21,11 @@ GOOD LUCK 😀
 const Data1 = [5, 2, 4, 1, 15, 8, 3];
 const Data2 = [16, 6, 10, 5, 6, 1, 4];
 
-const calcAverageHumanAge = ages => {
-  const averageAge = ages
+const calcAverageHumanAge = ages =>
+  ages
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
     .filter(age => age > 18)
     .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-  return averageAge;
-};
 
 console.log(calcAverageHumanAge(Data1));
 console.log(calcAverageHumanAge(Data2));
