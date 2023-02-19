90e4f5349566592d9161ad1c70a2528f321ecf80
DieWerkself <redead29@gmail.com>
2023-02-19 13:54:53 +0300

    Working with BigInt

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -468,7 +468,7 @@ labelBalance.addEventListener('click', () => {
     if (!(i % 3)) row.style.backgroundColor = 'blue';
   });
 });
-*/
+
 
 // 287,460,000,000
 const diameter = 287_460_000_000;
@@ -485,3 +485,35 @@ console.log(PI);
 
 console.log(Number('230_000'));
 console.log(parseInt('230_000'));
+*/
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(2876556429461594161649645645841n);
+console.log(BigInt(287655642946159));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(545448941618916518911840616151n * 1000000000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 21561651681651916168416n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n, typeof 20);
+console.log(20n == '20');
+
+console.log(huge + ' is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
