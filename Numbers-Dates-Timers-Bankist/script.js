d914663c3162e60014207b05ebcf21505d1cfad8
DieWerkself <redead29@gmail.com>
2023-02-19 13:36:00 +0300

    Numeric Separators

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -469,3 +469,19 @@ labelBalance.addEventListener('click', () => {
   });
 });
 */
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCencts = 345_99;
+console.log(priceCencts);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
