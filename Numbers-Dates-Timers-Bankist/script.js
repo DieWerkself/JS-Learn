2d1c28d73b62da7ea22568150ec1c9563e466fa0
DieWerkself <redead29@gmail.com>
2023-02-19 13:26:26 +0300

    The Remainder Operator

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -398,7 +398,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
-*/
+
 
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
@@ -439,3 +439,33 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 + 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+// Even
+console.log(6 % 2);
+console.log(6 / 2);
+
+// Odd
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => (n % 2 ? console.log('ODD') : console.log('EVEN'));
+isEven(3);
+isEven(4);
+isEven(454);
+
+labelBalance.addEventListener('click', () => {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    // 0, 2, 4, 6
+    if (!(i % 2)) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (!(i % 3)) row.style.backgroundColor = 'blue';
+  });
+});
+*/
